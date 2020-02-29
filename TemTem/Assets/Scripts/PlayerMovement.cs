70786f2c45fab1f7c9c4b0fca2d708a0d4474500
ErronStone <wyatta3998@gmail.com>
2020-02-28 23:26:53 -0500

    hallway project

---
+++ TemTem/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public int gem = 0;
+    Vector3 velocity;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        
+    }
+     void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hello");
+        if
+            (other.gameObject.CompareTag ("Pickup"))
+        {
+            Destroy(other.gameObject);
+            gem ++;
+              SceneManager.LoadScene(3);
+        }
+        
+        if
+            (other.gameObject.CompareTag ("Enemy"))
+        {
+            SceneManager.LoadScene(2); //endgame state
+        } 
+
+        
+    }
+}
---
+++ TemTem/Assets/Scripts/Sound.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioSource Step;
+    public AudioSource Gem;
+    public AudioSource bossmusic;
+    public AudioSource normalmusic;
+    public AudioClip Gemsound;
+void Start()
+{
+   
+}
+public void PlayStep()
+{
+    Step.Play ();
+}
+public void PlayGem()
+{
+    Gem.PlayOneShot(Gemsound, 1.0f);
+}
+public void Playbossmusic()
+{
+    bossmusic.Play ();
+}
+public void Playnormalmusic()
+{
+    normalmusic.Play ();
+}
+void OnTriggerEnter ()
+{
+    normalmusic.Stop ();
+    bossmusic.Play ();
+    
+}
+ void OnTriggerEnter(Collider other)
+    { 
+        if
+            (other.gameObject.CompareTag ("Pickup"))
+        {
+            Gem.PlayOneShot(Gemsound, 1.0f);
+        }
+    }    
+}
